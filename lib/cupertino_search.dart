6283c54e2da7e636ab0278feb41cdf660df57acd
Philip Chu <atlassian@technicat.com>
2021-01-24 22:22:30 -0800

    got it working

--- lib/cupertino_search.dart
+++ lib/cupertino_search.dart
@@ -1,7 +1,332 @@
 library cupertino_search;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:meta/meta.dart';
+
+typedef String FormFieldFormatter<T>(T v);
+typedef bool CupertinoSearchFilter<T>(T v, String c);
+typedef int CupertinoSearchSort<T>(T a, T b, String c);
+typedef Future<List<CupertinoSearchResult>> CupertinoResultsFinder(String c);
+typedef void OnSubmit(String value);
+
+class CupertinoSearchResult<T> extends StatelessWidget {
+  const CupertinoSearchResult({
+    Key key,
+    this.value,
+    this.text,
+    this.icon,
+  }) : super(key: key);
+
+  final T value;
+  final String text;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      child: new Row(
+        children: <Widget>[
+          new Container(width: 70.0, child: new Icon(icon)),
+          new Expanded(child: new Text(text, style: CupertinoTheme.of(context).textTheme.navTitleTextStyle)),
+        ],
+      ),
+      height: 56.0,
+    );
+  }
+}
+
+class CupertinoSearch<T> extends StatefulWidget {
+  CupertinoSearch({
+    Key key,
+    this.placeholder,
+    this.results,
+    this.getResults,
+    this.filter,
+    this.sort,
+    this.limit: 10,
+    this.onSelect,
+    this.onSubmit,
+    this.barBackgroundColor = CupertinoColors.white,
+    this.iconColor = CupertinoColors.black,
+    this.leading,
+  }) : assert(() {
+         if (results == null && getResults == null
+             || results != null && getResults != null) {
+           throw new AssertionError('Either provide a function to get the results, or the results.');
+         }
+
+         return true;
+       }()),
+       super(key: key);
+
+  final String placeholder;
+
+  final List<CupertinoSearchResult<T>> results;
+  final CupertinoResultsFinder getResults;
+  final CupertinoSearchFilter<T> filter;
+  final CupertinoSearchSort<T> sort;
+  final int limit;
+  final ValueChanged<T> onSelect;
+  final OnSubmit onSubmit;
+  final Color barBackgroundColor;
+  final Color iconColor;
+  final Widget leading;
+
+  @override
+  _CupertinoSearchState<T> createState() => new _CupertinoSearchState<T>();
+}
+
+class _CupertinoSearchState<T> extends State<CupertinoSearch> {
+  bool _loading = false;
+  List<CupertinoSearchResult<T>> _results = [];
+
+  String _criteria = '';
+  TextEditingController _controller = new TextEditingController();
+
+  _filter(dynamic v, String c) {
+    return v.toString().toLowerCase().trim()
+      .contains(new RegExp(r'' + c.toLowerCase().trim() + ''));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (widget.getResults != null) {
+      _getResultsDebounced();
+    }
+
+    _controller.addListener(() {
+      setState(() {
+        _criteria = _controller.value.text;
+        if (widget.getResults != null) {
+          _getResultsDebounced();
+        }
+      });
+    });
+  }
+
+  Timer _resultsTimer;
+  Future _getResultsDebounced() async {
+    if (_results.length == 0) {
+      setState(() {
+        _loading = true;
+      });
+    }
+
+    if (_resultsTimer != null && _resultsTimer.isActive) {
+      _resultsTimer.cancel();
+    }
+
+    _resultsTimer = new Timer(new Duration(milliseconds: 400), () async {
+      if (!mounted) {
+        return;
+      }
+
+      setState(() {
+        _loading = true;
+      });
+
+      //TODO: debounce widget.results too
+      var results = await widget.getResults(_criteria);
+
+      if (!mounted) {
+        return;
+      }
+
+      setState(() {
+        _loading = false;
+        _results = results;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _resultsTimer?.cancel();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var results = (widget.results ?? _results)
+      .where((CupertinoSearchResult result) {
+        if (widget.filter != null) {
+          return widget.filter(result.value, _criteria);
+        }
+        //only apply default filter if used the `results` option
+        //because getResults may already have applied some filter if `filter` option was omited.
+        else if (widget.results != null) {
+          return _filter(result.value, _criteria);
+        }
+
+        return true;
+      })
+      .toList();
+
+    if (widget.sort != null) {
+      results.sort((a, b) => widget.sort(a.value, b.value, _criteria));
+    }
+
+    results = results
+      .take(widget.limit)
+      .toList();
+
+   // IconThemeData iconTheme = CupertinoTheme.of(context).iconTheme.copyWith(color: widget.iconColor);
+
+    return new CupertinoPageScaffold(
+      navigationBar: new CupertinoNavigationBar(
+        leading: widget.leading,
+        backgroundColor: widget.barBackgroundColor,
+      //  iconTheme: iconTheme,
+        middle: CupertinoTextField(
+          controller: _controller,
+          autofocus: true,
+         // decoration: new InputDecoration.collapsed(hintText: widget.placeholder),
+          style: CupertinoTheme.of(context).textTheme.navLargeTitleTextStyle, // .title,
+          onSubmitted: (String value) {
+            if (widget.onSubmit != null) {
+              widget.onSubmit(value);
+            }
+          },
+        ),
+        trailing: _criteria.length == 0 ? null : 
+          CupertinoButton(
+            child: Icon(CupertinoIcons.clear),
+            onPressed: () {
+              setState(() {
+                _controller.text = _criteria = '';
+              });
+            }
+          ),
+        
+      ),
+     child: _loading
+        ? new Center(
+            child: new Padding(
+              padding: const EdgeInsets.only(top: 50.0),
+              child: CupertinoActivityIndicator() 
+            ),
+          )
+        : new SingleChildScrollView(
+            child: new Column(
+              children: results.map((CupertinoSearchResult result) {
+                return new CupertinoButton(
+                  onPressed: () => widget.onSelect(result.value),
+                  child: result,
+                );
+              }).toList(),
+            ),
+          ),
+    );
+  }
+}
+
+class _CupertinoSearchPageRoute<T> extends CupertinoPageRoute<T> {
+  _CupertinoSearchPageRoute({
+    @required WidgetBuilder builder,
+    RouteSettings settings: const RouteSettings(),
+    maintainState: true,
+    bool fullscreenDialog: false,
+  }) : assert(builder != null),
+        super(builder: builder, settings: settings, maintainState: maintainState, fullscreenDialog: fullscreenDialog);
+}
+
+class CupertinoSearchInput<T> extends StatefulWidget {
+  CupertinoSearchInput({
+    Key key,
+    this.onSaved,
+    this.validator,
+    this.placeholder,
+    this.formatter,
+    this.results,
+    this.getResults,
+    this.filter,
+    this.sort,
+    this.onSelect,
+  });
+
+  final FormFieldSetter<T> onSaved;
+  final FormFieldValidator<T> validator;
+  final String placeholder;
+  final FormFieldFormatter<T> formatter;
+
+  final List<CupertinoSearchResult<T>> results;
+  final CupertinoResultsFinder getResults;
+  final CupertinoSearchFilter<T> filter;
+  final CupertinoSearchSort<T> sort;
+  final ValueChanged<T> onSelect;
+
+  @override
+  _CupertinoSearchInputState<T> createState() => new _CupertinoSearchInputState<T>();
+}
+
+class _CupertinoSearchInputState<T> extends State<CupertinoSearchInput<T>> {
+  GlobalKey<FormFieldState<T>> _formFieldKey = new GlobalKey<FormFieldState<T>>();
+
+  _buildCupertinoSearchPage(BuildContext context) {
+    return new _CupertinoSearchPageRoute<T>(
+      settings: new RouteSettings(
+        name: 'cupertino_search',
+      ),
+      builder: (BuildContext context) {
+        return CupertinoSearch<T>(
+            placeholder: widget.placeholder,
+            results: widget.results,
+            getResults: widget.getResults,
+            filter: widget.filter,
+            sort: widget.sort,
+            onSelect: (dynamic value) => Navigator.of(context).pop(value),
+        );
+      }
+    );
+  }
+
+  _showCupertinoSearch(BuildContext context) {
+    Navigator.of(context)
+      .push(_buildCupertinoSearchPage(context))
+      .then((dynamic value) {
+        if (value != null) {
+          _formFieldKey.currentState.didChange(value);
+          widget.onSelect(value);
+        }
+      });
+  }
+
+  bool _isEmpty(field) {
+    return field.value == null;
+  }
+
+  Widget build(BuildContext context) {
+    final TextStyle valueStyle = CupertinoTheme.of(context).textTheme.navTitleTextStyle; //.subhead;
+
+    return CupertinoButton(
+      onPressed: () => _showCupertinoSearch(context),
+      child: new FormField<T>(
+        key: _formFieldKey,
+        validator: widget.validator,
+        onSaved: widget.onSaved,
+        autovalidateMode: AutovalidateMode.always,
+        builder: (FormFieldState<T> field) {
+          return new  Container(
+           /* baseStyle: valueStyle,
+            isEmpty: _isEmpty(field),
+            decoration: new Container(
+              labelStyle: _isEmpty(field) ? null : valueStyle,
+              labelText: widget.placeholder,
+              errorText: field.errorText,
+            ), */
+            child: _isEmpty(field) ? null : new Text(
+              widget.formatter != null
+                ? widget.formatter(field.value)
+                : field.value.toString(),
+              style: valueStyle
+            ),
+          );
+        },
+      ),
+    );
+  }
 }
