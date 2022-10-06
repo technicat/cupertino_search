7c6f6f1a18b225bf5a3476a12754deb1ff37b3c0
Philip Chu <technicat@users.noreply.github.com>
2022-10-06 15:41:05 -0700

    fix test compilation

--- test/cupertino_search_test.dart
+++ test/cupertino_search_test.dart
@@ -34,18 +34,16 @@ void main() {
       new StatefulBuilder(
         builder: (BuildContext context, StateSetter setState) {
           return new CupertinoApp(
-            home: new StatefulWidget(
-              child: new CupertinoSearch(
-                placeholder: 'Find something',
-                results: _names
-                    .map((String v) => new CupertinoSearchResult<String>(
-                          icon: Icons.person,
-                          value: v,
-                          text: v,
-                        ))
-                    .toList(),
-                onSelect: selectMock.onSelect,
-              ),
+            home: new CupertinoSearch(
+              placeholder: 'Find something',
+              results: _names
+                  .map((String v) => new CupertinoSearchResult<String>(
+                        icon: CupertinoIcons.person,
+                        value: v,
+                        text: v,
+                      ))
+                  .toList(),
+              onSelect: selectMock.onSelect,
             ),
           );
         },
@@ -57,14 +55,14 @@ void main() {
     });
 
     expect(find.text('Find something'), findsOneWidget);
-    expect(find.byIcon(Icons.clear), findsNothing);
+    expect(find.byIcon(CupertinoIcons.clear), findsNothing);
 
-    await tester.enterText(find.byType(TextField), _names[2]);
+    await tester.enterText(find.byType(CupertinoTextField), _names[2]);
     await tester.pump();
 
     //clear button, to empty the search
     //only shown when some text is typed
-    expect(find.byIcon(Icons.clear), findsOneWidget);
+    expect(find.byIcon(CupertinoIcons.clear), findsOneWidget);
 
     expect(
         find.text(_names[2]), findsNWidgets(2)); //the text input and the result
