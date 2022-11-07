63c9c1bf74fd53a4d21c7a92efd053b6f23dd0b5
Philip Chu <technicat@users.noreply.github.com>
2022-11-06 20:02:26 -0800

    cleanup

--- test/cupertino_search_test.dart
+++ test/cupertino_search_test.dart
@@ -1,9 +1,3 @@
-// This is a basic Flutter widget test.
-// To perform an interaction with a widget in your test, use the WidgetTester utility that Flutter
-// provides. For example, you can send tap and scroll gestures. You can also use WidgetTester to
-// find child widgets in the widget tree, read text, and verify that the values of widget properties
-// are correct.
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
@@ -70,8 +64,6 @@ void main() {
     _names.where((String name) => name != _names[2]).forEach((String name) {
       expect(find.text(name), findsNothing);
     });
-
-    //TODO: tap on the first result and assert onSelect has been called
 
     return;
   });
